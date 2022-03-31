fb21ef37d56aaed84fc151c683c159b7e2865b02
Alero <alero@Ashanti.local>
2022-03-31 14:00:49 +0300

    modified keyevents to so wasdqe moves light

--- src/keyevents.c
+++ src/keyevents.c
@@ -6,7 +6,7 @@
 /*   By: esukava <esukava@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 21:12:37 by esukava           #+#    #+#             */
-/*   Updated: 2022/02/23 20:58:06 by esukava          ###   ########.fr       */
+/*   Updated: 2022/03/29 12:42:43 by alero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,18 @@ void	keyevent(t_rt *rt, SDL_Event *e)
 	{
 		if (e->type == SDL_KEYDOWN)
 		{
+			if (e->key.keysym.sym == SDLK_w)
+				rt->object[9].pos.z -= 5;
+			if (e->key.keysym.sym == SDLK_a)
+				rt->object[9].pos.x -= 5;
+			if (e->key.keysym.sym == SDLK_s)
+				rt->object[9].pos.z += 5;
+			if (e->key.keysym.sym == SDLK_d)
+				rt->object[9].pos.x += 5;
+			if (e->key.keysym.sym == SDLK_q)
+				rt->object[9].pos.y += 5;
+			if (e->key.keysym.sym == SDLK_e)
+				rt->object[9].pos.y -= 5;
 			if (e->key.keysym.sym == SDLK_ESCAPE)
 				rt->rend.run = FALSE;
 			rt->cam.dir = v_rot_xyz(rt->cam.dir, rt->cam.rot);
