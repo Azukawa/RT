3e4c545516eac1a9b8b01077b0f2ef0056ab60bc
Alero <alero@Ashanti.local>
2022-10-06 16:11:40 +0300

    plane uv-mapping works. Not even close to norme

--- src/textures.c
+++ src/textures.c
@@ -6,7 +6,7 @@
 /*   By: alero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 15:20:56 by alero             #+#    #+#             */
-/*   Updated: 2022/09/30 19:44:13 by alero            ###   ########.fr       */
+/*   Updated: 2022/10/06 16:09:18 by alero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,11 +55,54 @@ static void	spherical_map(t_rt *rt, t_fvector hp, t_fvector pos)
 	rt->uv_v = 0.5f + (asinf(n.y) / M_PI);
 	rt->uv_v *= 0.5;
 }
+/*
+t_fvector	v_multiply(t_fvector a, t_fvector b)
+{
+t_vec	m3_multiply_vec(t_m3 m, t_vec v)
+{
+	t_vec	ret;
 
+	ret.x = v.x * m.m[0][0] + v.y * m.m[0][1] + v.z * m.m[0][2];
+	ret.y = v.x * m.m[1][0] + v.y * m.m[1][1] + v.z * m.m[1][2];
+	ret.z = v.x * m.m[2][0] + v.y * m.m[2][1] + v.z * m.m[2][2];
+	return (ret);
+}
+}
+*/
 static void	planar_map(t_rt *rt, t_fvector hp)
 {
-	rt->uv_u = fmodf(hp.x, 1.0f);
-	rt->uv_v = fmodf(hp.y, 1.0f);
+	t_fvector n;
+	static int bing;
+	t_fvector	e1, e2;
+	n = rt->object[rt->curobj].dir;
+	if(bing == 100)
+	{
+		printf("n.x = %f\tn.y = %f\tn.z = %f\n", n.x, n.y, n.z);
+		printf("hp.x = %f\thp.y = %f\thp.z = %f\n", hp.x, hp.y, hp.z);
+	}
+//	n = v_sub(n, (t_fvector){0, 1, 0});
+//	n = (t_fvector){RAD_TO_DEG * n.x, RAD_TO_DEG * n.y, RAD_TO_DEG * n.z};
+//	n = v_rot_xyz(hp, n);
+	e1 = v_normalize(v_cross(n, (t_fvector){1, 0, 0}));
+	e2 = v_normalize(v_cross(n, e1));
+//	if(bing == 100)
+//		printf("n.x = %f\tn.y = %f\tn.z = %f\n", n.x, n.y, n.z);
+//	rt->uv_u = fmodf(n.x, 1.0f); //og
+//	rt->uv_v = fmodf(n.z, 1.0f); //og
+
+
+
+	rt->uv_u = v_dot(e1, hp); 
+	rt->uv_v = v_dot(e2, hp);
+//	if(bing == 100){
+//		printf("cam posx = %f\tray_startx = %f\n", rt->cam.pos.x, rt->r_prm.start.x);
+//		printf("cam posy = %f\tray_starty = %f\n", rt->cam.pos.y, rt->r_prm.start.y);
+//	}
+//	rt->uv_u = rt->cam.pos.x + rt->r_prm.dir.x * iso_t;
+//	rt->uv_v = rt->cam.pos.y + rt->r_prm.dir.y * iso_t;
+
+
+	bing++;
 }
 
 static void	cylindrical_map(t_rt *rt, t_fvector pos)
